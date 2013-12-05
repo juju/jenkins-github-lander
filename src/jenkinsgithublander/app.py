884437d905d0c146fb5202a58392c74d3ece0e63
Richard Harding <rharding@mitechie.com>
2013-12-05 11:15:24 -0500

    Add a index page to list projects being watched

--- src/jenkinsgithublander/app.py
+++ src/jenkinsgithublander/app.py
@@ -6,6 +6,26 @@ from jenkinsgithublander.utils import build_config
 
 
 # Route callables
+def home(request):
+    settings = request.registry.settings
+    html = """
+    <h2>Watching Github Projects</h2>
+    <div>Owner: {owner}</div>
+    <ul>
+        {projects}
+    </ul>
+
+    """.format(
+        owner=settings['github.owner'],
+        projects='\n'.join([
+            '<li>{}</li>'.format(proj) for proj in settings['projects']
+        ]),
+
+    )
+
+    return Response(html)
+
+
 def trigger_mergable_commits(request):
     config = request.registry.settings
     kicked = kick_mergeable_pull_requests(config)
@@ -22,6 +42,8 @@ def main(global_config, **settings):
     settings = build_config(settings)
     config = Configurator(settings=settings)
 
+    config.add_route('home', '/')
+    config.add_view(home, route_name='home')
     config.add_route('check_pulls', '/check_pulls')
     config.add_view(trigger_mergable_commits, route_name='check_pulls')
 
